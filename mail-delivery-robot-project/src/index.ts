be048050907dd3e902ac527b0e8afe4513fbcf9e
Zoltán Völcsey <zvolcsey@gmail.com>
2022-08-29 12:21:36 +0200

    add roads and buildGraph function

--- mail-delivery-robot-project/src/index.ts
+++ mail-delivery-robot-project/src/index.ts
@@ -1,1 +1,37 @@
-console.log('Hello World!');
+const roads: string[] = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  'Marketplace-Farm',
+  'Marketplace-Post Office',
+  'Marketplace-Shop',
+  'Marketplace-Town Hall',
+  'Shop-Town Hall',
+];
+
+function buildGraph(edges: string[]) {
+  let graph = Object.create(null);
+
+  function addEdge(from: string, to: string) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+
+  for (let [from, to] of edges.map((r) => r.split('-'))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+
+  return graph;
+}
+
+console.log(buildGraph(roads));
